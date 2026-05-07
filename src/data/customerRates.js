4b0275e2091103316e726d995d18746edda87741
quintashipping-bit <quintashipping@gmail.com>
2026-05-07 12:43:13 +0100

    Create customerRates.js

---
+++ src/data/customerRates.js
@@ -0,0 +1,25 @@
+export const customerRates = [
+  {
+    customer: "ESKOM",
+    country: "SOUTH AFRICA",
+    zone: 350,
+    ratePerKg: 5,
+    surcharge: 550,
+  },
+
+  {
+    customer: "SWAKOP URANIUM",
+    country: "NAMIBIA",
+    zone: 700,
+    ratePerKg: 14.5,
+    surcharge: 550,
+  },
+
+  {
+    customer: "AGL LOY YANG A",
+    country: "AUSTRALIA",
+    zone: 2,
+    ratePerKg: 0,
+    surcharge: 0,
+  },
+];
