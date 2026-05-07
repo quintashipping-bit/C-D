2f9490dc34764bec04adad784d81df969fa6c2a3
quintashipping-bit <quintashipping@gmail.com>
2026-05-07 15:48:43 +0100

    Update main.jsx

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import '../styles/index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
+/* =====================================================
+   MAIN.JSX
+===================================================== */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import "./index.css";
+
+/* =====================================================
+   ROOT RENDER
+===================================================== */
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
