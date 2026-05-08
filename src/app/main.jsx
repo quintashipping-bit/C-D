01782f9cb36a72cba31fa6da2e8bd5748de16211
quintashipping-bit <quintashipping@gmail.com>
2026-05-08 07:11:23 +0100

    Update main.jsx

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "../src/styles/index.css";
+import "../styles/index.css";
 
 /* =====================================================
    ROOT RENDER
